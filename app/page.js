23186418410ce0b1b3d8b32f30019abe5a21377d
Anoopkaur24 <141067540+Anoopkaur24@users.noreply.github.com>
2024-08-02 18:19:43 -0600

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,11 +1,24 @@
-import Image from "next/image";
+import Image from 'next/image';
+import styles from '../styles/Home.module.css';
 
-export default function Home() {
+export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <h1>Hello</h1>
+    <div className={styles.container}>
+      <Image
+        src="/photos/profile.jpeg"
+        alt="Anoop Kaur"
+        width={200} // adjust the width as needed
+        height={200} // adjust the height as needed
+        className={`${styles.profilePhoto} fadeIn`}
+      />
+      <h1 className="fadeIn">Anoop Kaur</h1>
+      <p className="fadeIn">
+        Dedicated Work Study Student accustomed to performing effectively in both collaborative and autonomous capacities. Strong knowledge of various Programming languages and MS Office. Friendly and outgoing emerging professional.
+      </p>
+      <div className={styles.socialLinks}>
+        <a href="https://www.linkedin.com/in/anoop-kaur-48bb89278" target="_blank" className="fadeIn">LinkedIn</a>
+        <a href="https://github.com/Anoopkaur24" target="_blank" className="fadeIn">GitHub</a>
       </div>
-    </main>
+    </div>
   );
 }
